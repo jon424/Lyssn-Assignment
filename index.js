4d233563bac9024c23905e67227bbc592d14cc1f
Jon <jonathanjackson424@gmail.com>
2020-03-16 16:50:29 -0400

    reformatted code

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const data = [
-  {'name':'user1','group':1},
-  {'name':'user2','group':1},
-  {'name':'user3','group':2}
+	{ 'name': 'user1', 'group': 1 },
+	{ 'name': 'user2', 'group': 1 },
+	{ 'name': 'user3', 'group': 2 }
 ];
 
 console.log('top line', data.group);
@@ -10,29 +10,21 @@ console.log('top line', data.group);
 const app = document.querySelector('#app');
 
 // Create markup to render data to DOM
-app.innerHTML = '<div>' + data.map(function(user){
-	
-  return '<div id="div" class="username">' + user.name +
-	'<span name="span" id="hidden">' + user.name + ' is in group number ' + user.group + '</span>'
-	+ '</div>';
+app.innerHTML = '<div>' + data.map(function (user) {
+
+	return '<div id="div" class="username">' + user.name +
+		'<span name="span" id="hidden">' + user.name + ' is in group number ' + user.group + '</span>'
+		+ '</div>';
 }).join('') + '</div>';
 
 
-//document.getElementById('hidden').style.display = 'none';
-
 //Create Event Listener
 document.addEventListener('click', function (event) {
-  if (!event.target.matches('.username')) return;
-  
-  event.preventDefault();
-	
+	if (!event.target.matches('.username')) return;
 
-	
+	event.preventDefault();
 
-  //var x = event.getElementsById('hidden');
-  alert(event.target.getElementsByTagName('SPAN')[0].innerHTML);
-
-  //alert(event.target.getElementsByTagName('SPAN'));
+	alert(event.target.getElementsByTagName('SPAN')[0].innerHTML);
 
 }, false);
 
